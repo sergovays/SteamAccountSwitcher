5e640b6f0a1557bc3e4f7a25b485e0830bb26885
Felix <DukeBruno123@gmail.com>
2018-10-06 11:36:17 +0200

    Initial commit

---
+++ SteamAccountSwitcher/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace SteamAccountSwitcher
+{
+	partial class AccountSwitcher
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button = new System.Windows.Forms.Button();
+			this.label2 = new System.Windows.Forms.Label();
+			this.action = new System.Windows.Forms.Label();
+			this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+			this.SuspendLayout();
+			// 
+			// button
+			// 
+			this.button.Location = new System.Drawing.Point(12, 53);
+			this.button.Name = "button";
+			this.button.Size = new System.Drawing.Size(208, 23);
+			this.button.TabIndex = 0;
+			this.button.Text = "Generate New Account";
+			this.button.UseVisualStyleBackColor = true;
+			this.button.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(13, 37);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(40, 13);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "Status:";
+			// 
+			// action
+			// 
+			this.action.Anchor = System.Windows.Forms.AnchorStyles.Left;
+			this.action.AutoSize = true;
+			this.action.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			this.action.Location = new System.Drawing.Point(59, 37);
+			this.action.Name = "action";
+			this.action.Size = new System.Drawing.Size(24, 13);
+			this.action.TabIndex = 3;
+			this.action.Text = "Idle";
+			// 
+			// linkLabel1
+			// 
+			this.linkLabel1.AutoSize = true;
+			this.linkLabel1.Location = new System.Drawing.Point(13, 9);
+			this.linkLabel1.Name = "linkLabel1";
+			this.linkLabel1.Size = new System.Drawing.Size(121, 13);
+			this.linkLabel1.TabIndex = 4;
+			this.linkLabel1.TabStop = true;
+			this.linkLabel1.Text = "Created by: @BeepFelix";
+			this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+			// 
+			// AccountSwitcher
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(232, 85);
+			this.Controls.Add(this.linkLabel1);
+			this.Controls.Add(this.action);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.button);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+			this.MaximizeBox = false;
+			this.Name = "AccountSwitcher";
+			this.Text = "Account Switcher";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label action;
+		private System.Windows.Forms.LinkLabel linkLabel1;
+	}
+}
+
---
+++ SteamAccountSwitcher/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using Microsoft.Win32;
+using System.Json;
+
+namespace SteamAccountSwitcher
+{
+	public partial class AccountSwitcher : Form
+	{
+		public AccountSwitcher()
+		{
+			InitializeComponent();
+		}
+
+		private async void button1_Click(object sender, EventArgs e)
+		{
+			if (!this.action.Text.Equals("Idle"))
+			{
+				MessageBox.Show("Currently busy. Please wait until the status changes to \"Idle\"");
+				return;
+			}
+
+			// Terminate steam
+			this.action.Text = "Terminating Steam...";
+
+			try
+			{
+				foreach (Process proc in Process.GetProcessesByName("Steam"))
+				{
+					proc.Kill();
+				}
+			}
+			catch (Exception ex)
+			{
+				// Error while killing all "Steam" processes
+				MessageBox.Show(ex.Message);
+				this.action.Text = "Idle";
+				return;
+			}
+
+			// Generate account
+			this.action.Text = "Generating new account...";
+
+			string responseString = null;
+
+			HttpClient client = new HttpClient();
+			try
+			{
+				responseString = await client.GetStringAsync("http://accgen.undo.it/steam/apit");
+			}
+			catch(Exception ex)
+			{
+				// Error while requesting the page
+				MessageBox.Show(ex.Message);
+				this.action.Text = "Idle";
+				return;
+			}
+
+			JsonValue json = null;
+			try
+			{
+				json = JsonValue.Parse(responseString);
+			}
+			catch (Exception ex)
+			{
+				// Error while parsing the response
+				MessageBox.Show(ex.Message);
+				this.action.Text = "Idle";
+				return;
+			}
+
+			if (!json.ContainsKey("success") || json["success"] != 1 || !json.ContainsKey("username") || !json.ContainsKey("password"))
+			{
+				// Result is not a success, does not contain "username" or does not contain "password"
+				MessageBox.Show(responseString);
+				this.action.Text = "Idle";
+				return;
+			}
+
+			string username = json["username"];
+			string password = json["password"];
+
+			if (username == null || password == null)
+			{
+				// Failed to find "username" or "password" (Should never be triggered as the top already checks for this)
+				MessageBox.Show(responseString);
+				this.action.Text = "Idle";
+				return;
+			}
+
+			// Start Steam and login
+			this.action.Text = "Starting Steam...";
+			
+			// Find Steam installation path. First GetValue() is for 64-Bit, second one is for 32-Bit
+			var r = Registry.GetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Valve\Steam", "InstallPath", null);
+			if (r == null)
+			{
+				r = Registry.GetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Valve\Steam", "InstallPath", null);
+				if (r == null)
+				{
+					// Steam is not installed
+					MessageBox.Show("Could not find Steam installation path");
+					this.action.Text = "Idle";
+					return;
+				}
+			}
+
+			// https://developer.valvesoftware.com/wiki/Command_Line_Options#Steam_.28Windows.29
+			ProcessStartInfo startInfo = new ProcessStartInfo();
+			startInfo.FileName = r.ToString() + "/steam.exe";
+			startInfo.Arguments = "-login " + username + " " + password;
+			Process.Start(startInfo);
+
+			// Return to waiting
+			this.action.Text = "Idle";
+		}
+
+		private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+		{
+			this.linkLabel1.LinkVisited = true;
+			System.Diagnostics.Process.Start("https://t.me/BeepFelix");
+		}
+	}
+}
